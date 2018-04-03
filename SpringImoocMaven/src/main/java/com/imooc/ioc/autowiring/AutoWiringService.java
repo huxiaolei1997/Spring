5284dcd2a16671d5fd4ea244fae9502dc8f082f0
xiaolei hu <1245976894@qq.com>
2018-04-03 10:14:38 +0800

    update

--- SpringImoocMaven/src/main/java/com/imooc/ioc/autowiring/AutoWiringService.java
+++ SpringImoocMaven/src/main/java/com/imooc/ioc/autowiring/AutoWiringService.java
@@ -3,11 +3,13 @@ package com.imooc.ioc.autowiring;
 public class AutoWiringService {
     private AutoWiringDAO autoWiringDAO;
 
-//    public AutoWiringService(AutoWiringDAO autoWiringDAO) {
-//        System.out.println("AutoWiringService");
-//        this.autoWiringDAO = autoWiringDAO;
-//    }
+    // default-autowire 自动注入 constructor方式
+    public AutoWiringService(AutoWiringDAO autoWiringDAO) {
+        System.out.println("AutoWiringService");
+        this.autoWiringDAO = autoWiringDAO;
+    }
 
+    // byName, byType 自动注入方式
     public void setAutoWiringDAO(AutoWiringDAO autoWiringDAO) {
         System.out.println("setAutoWiringDAO");
         this.autoWiringDAO = autoWiringDAO;
