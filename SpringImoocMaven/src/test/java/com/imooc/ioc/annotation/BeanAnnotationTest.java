7a79ba474aa8a37e254ace695377505c539f397a
xiaolei hu <1245976894@qq.com>
2018-04-04 22:36:14 +0800

    bean管理的注解及实现例子

---
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/BeanAnnotationTest.java
@@ -0,0 +1,36 @@
+package com.imooc.ioc.annotation;
+
+import junit.framework.TestCase;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanAnnotationTest extends TestCase {
+
+    public void testSay() {
+        String springXmlPath = "classpath*:spring/spring-ioc.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        BeanAnnotation beanAnnotation = abstractApplicationContext.getBean("beanAnnotation", BeanAnnotation.class);
+        beanAnnotation.say("This is a test.");
+        abstractApplicationContext.close();
+    }
+
+    public void testMyHashCode() {
+        String springXmlPath = "classpath*:spring/spring-ioc.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        BeanAnnotation beanAnnotation = abstractApplicationContext.getBean("beanAnnotation", BeanAnnotation.class);
+        BeanAnnotation beanAnnotation2 = abstractApplicationContext.getBean("beanAnnotation", BeanAnnotation.class);
+        beanAnnotation.myHashCode();
+        beanAnnotation2.myHashCode();
+        abstractApplicationContext.close();
+    }
+}
