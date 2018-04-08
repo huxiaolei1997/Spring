f695577876d08471e43cf8d6050c16f97dc99fbb
xiaolei hu <1245976894@qq.com>
2018-04-08 17:05:59 +0800

    基于java的容器注解（test）

---
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
@@ -0,0 +1,24 @@
+package com.imooc.ioc.annotation.javabased;
+
+import com.imooc.ioc.annotation.injection.InjectionService;
+import junit.framework.TestCase;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StoreConfigTest extends TestCase {
+
+    public void testGetStringStore() {
+        String springXmlPath = "classpath*:spring/spring-beanannotation.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        // 如果在@Configuration注解的类中没有指定Bean的name,那么getBean方法中的name默认为@Bean注解的方法名，这里是getStringStore
+        Store store = abstractApplicationContext.getBean("stringStore", Store.class);
+        //injectionService.sayHelloWorld("hello, world");
+        System.out.println(store.getClass().getName());
+        abstractApplicationContext.close();
+    }
+}
