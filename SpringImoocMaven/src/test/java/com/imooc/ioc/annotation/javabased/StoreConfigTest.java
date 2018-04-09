a93d77b11452970c63f962d957028e977bdb2602
xiaolei hu <1245976894@qq.com>
2018-04-09 19:50:54 +0800

    Spring Bean装配之基于Java的容器注解说明——@ImportResource和@Value

--- SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
@@ -21,4 +21,18 @@ public class StoreConfigTest extends TestCase {
         System.out.println(store.getClass().getName());
         abstractApplicationContext.close();
     }
+
+    public void testMyDriverManager() {
+        String springXmlPath = "classpath*:spring/spring-beanannotation.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        MyDriverManager myDriverManager = abstractApplicationContext.getBean("myDriverManager", MyDriverManager.class);
+        System.out.println(myDriverManager.getClass().getName());
+        abstractApplicationContext.close();
+
+    }
 }
