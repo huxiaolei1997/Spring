f3d47272ac9e531a047d79fce9088d2f626f2db0
xiaolei hu <1245976894@qq.com>
2018-04-09 19:52:27 +0800

    Spring Bean装配之基于Java的容器注解说明——@ImportResource和@Value

--- SpringImoocMaven/src/main/java/com/imooc/ioc/annotation/javabased/StoreConfig.java
+++ SpringImoocMaven/src/main/java/com/imooc/ioc/annotation/javabased/StoreConfig.java
@@ -1,11 +1,30 @@
 package com.imooc.ioc.annotation.javabased;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+// 这是一个配置类，类似于xml文件
 @Configuration
+@ImportResource("classpath:jdbc/config.xml")
 public class StoreConfig {
+    @Value("${url}")
+    private String url;
 
+    // 在jdbc.properties中需要注意的是，不能写user_name，否则获取到的是登录当前操作系统的用户名，可以改成jdbc.user_name
+    @Value("${jdbc.username}")
+    private String user_name;
+
+    @Value("${password}")
+    private String password;
+
+    @Bean
+    public MyDriverManager myDriverManager() {
+        return new MyDriverManager(url, user_name, password);
+    }
+
+    // 默认bean是单例（singleton），我们可以自己指定
     @Bean(name = "stringStore", initMethod = "init", destroyMethod = "destroy")
     public Store getStringStore() {
         return new StringStoreImpl();
