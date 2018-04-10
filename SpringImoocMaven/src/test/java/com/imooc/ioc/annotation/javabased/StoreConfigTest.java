d11bb7753d3a81b58f69453732ac69e92b1702aa
xiaolei hu <1245976894@qq.com>
2018-04-10 22:44:37 +0800

    基于Java的容器注解说明——@Bean和@Scope

--- SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
@@ -15,10 +15,29 @@ public class StoreConfigTest extends TestCase {
         // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
         AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
         abstractApplicationContext.start();
-        // 如果在@Configuration注解的类中没有指定Bean的name,那么getBean方法中的name默认为@Bean注解的方法名，这里是getStringStore
+        // 如果在@Configuration注解的类中没有指定Bean的name,那么getBean方法中的name默认为@Bean注解的方法名，这里的bean已经指定了name,所以是 stringStore
         Store store = abstractApplicationContext.getBean("stringStore", Store.class);
         //injectionService.sayHelloWorld("hello, world");
         System.out.println(store.getClass().getName());
+        abstractApplicationContext.close();
+    }
+
+    public void testStringStore2() {
+        String springXmlPath = "classpath*:spring/spring-beanannotation.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        // 如果在@Configuration注解的类中没有指定Bean的name,那么getBean方法中的name默认为@Bean注解的方法名，这里的bean已经指定了name,所以是 stringStore2
+        Store store = abstractApplicationContext.getBean("stringStore2", Store.class);
+        Store store2 = abstractApplicationContext.getBean("stringStore2", Store.class);
+        //injectionService.sayHelloWorld("hello, world");
+        System.out.println("--store start--");
+        System.out.println(store.hashCode());
+        System.out.println(store2.hashCode());
+        System.out.println("--store end--");
         abstractApplicationContext.close();
     }
 
@@ -35,4 +54,5 @@ public class StoreConfigTest extends TestCase {
         abstractApplicationContext.close();
 
     }
+
 }
