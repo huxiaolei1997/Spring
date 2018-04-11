ca420f2946642ee1bcd9c6f7a695b017d1dcdb16
xiaolei hu <1245976894@qq.com>
2018-04-11 19:36:15 +0800

    update 基于泛型的自动装配

--- SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/javabased/StoreConfigTest.java
@@ -55,4 +55,19 @@ public class StoreConfigTest extends TestCase {
 
     }
 
+    public void testStringStore3() {
+        String springXmlPath = "classpath*:spring/spring-beanannotation.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        // 如果在@Configuration注解的类中没有指定Bean的name,那么getBean方法中的name默认为@Bean注解的方法名，这里的bean已经指定了name,所以是 stringStore
+        StringStoreImpl store = abstractApplicationContext.getBean("stringStoreTest", StringStoreImpl.class);
+        //injectionService.sayHelloWorld("hello, world");
+        //System.out.println(store.getClass().getName());
+        abstractApplicationContext.close();
+    }
+
 }
