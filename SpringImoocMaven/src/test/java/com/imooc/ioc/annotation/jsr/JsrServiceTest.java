14043766a2ff79aabc5a6e9afc89a1d1f9b6e528
xiaolei hu <huxiaolei1997@gmail.com>
2018-04-12 20:06:54 +0800

    Spring Bean装配之Spring对JSR支持的说明 测试类

---
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/annotation/jsr/JsrServiceTest.java
@@ -0,0 +1,23 @@
+package com.imooc.ioc.annotation.jsr;
+
+import com.imooc.ioc.annotation.javabased.MyDriverManager;
+import junit.framework.TestCase;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JsrServiceTest extends TestCase {
+
+    public void testSave() {
+        String springXmlPath = "classpath*:spring/spring-beanannotation.xml";
+        // 非web应用使用AbstractApplicationContext初始化bean容器
+        // 在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，
+        // 要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。
+        // 如果不记得关闭容器，最典型的问题就是数据库连接不能释放。
+        AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext(springXmlPath);
+        abstractApplicationContext.start();
+        JsrService jsrService = abstractApplicationContext.getBean("jsrService", JsrService.class);
+        jsrService.save();
+        //System.out.println(myDriverManager.getClass().getName());
+        abstractApplicationContext.close();
+    }
+}
