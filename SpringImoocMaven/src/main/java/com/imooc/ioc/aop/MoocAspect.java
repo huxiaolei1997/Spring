047d7560f6778fe0d95377e845b250db94d18b27
xiaolei hu <huxiaolei1997@gmail.com>
2018-04-14 23:16:40 +0800

    advice应用（上）

--- SpringImoocMaven/src/main/java/com/imooc/ioc/aop/MoocAspect.java
+++ SpringImoocMaven/src/main/java/com/imooc/ioc/aop/MoocAspect.java
@@ -1,4 +1,22 @@
 package com.imooc.ioc.aop;
 
+import org.aspectj.lang.JoinPoint;
+
 public class MoocAspect {
+
+    public void before() {
+        System.out.println("MoocAspect before");
+    }
+
+    public void afterReturning() {
+        System.out.println("MoocAspect afterReturning");
+    }
+
+    public void afterThrowing() {
+        System.out.println("MoocAspect afterThrowing");
+    }
+
+    public void after() {
+        System.out.println("MoocAspect after");
+    }
 }
---
+++ SpringImoocMaven/src/main/java/com/imooc/ioc/aop/biz/AspectBiz.java
@@ -0,0 +1,9 @@
+package com.imooc.ioc.aop.biz;
+
+public class AspectBiz {
+
+    public void biz() {
+        System.out.println("AspectBiz biz");
+        //throw new RuntimeException();
+    }
+}
