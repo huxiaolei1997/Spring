a58e9b3090fb2ab97fb45bca746ee6ab089e0735
xiaolei hu <huxiaolei1997@gmail.com>
2018-05-05 12:26:05 +0800

    AspectJ介绍及Pointcut注解应用

---
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/test/TestInterface.java
@@ -0,0 +1,13 @@
+package com.imooc.ioc.test;
+
+public abstract class TestInterface {
+    abstract void test();
+
+    abstract void test2();
+
+    abstract void test3();
+
+    void test4() {
+
+    }
+}
---
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/test/TestInterfaceImpl.java
@@ -0,0 +1,19 @@
+package com.imooc.ioc.test;
+
+public class TestInterfaceImpl extends TestInterface {
+
+    @Override
+    void test() {
+
+    }
+
+    @Override
+    void test2() {
+
+    }
+
+    @Override
+    void test3() {
+
+    }
+}
