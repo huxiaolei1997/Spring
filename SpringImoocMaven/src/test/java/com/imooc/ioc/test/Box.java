93c77c52abf0f134ea5db3d706b3ebb1379e05b6
xiaolei hu <huxiaolei1997@gmail.com>
2018-05-20 10:53:20 +0800

    对象的序列化以及解序列化

--- SpringImoocMaven/src/test/java/com/imooc/ioc/test/Box.java
+++ SpringImoocMaven/src/test/java/com/imooc/ioc/test/Box.java
@@ -1,9 +1,6 @@
 package com.imooc.ioc.test;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
 public class Box implements Serializable {
     private int width;
@@ -21,12 +18,26 @@ public class Box implements Serializable {
         Box box = new Box();
         box.setWidth(50);
         box.setHeight(30);
-
+        // 序列化对象
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("box.ser");
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(box);
             objectOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 解序列化对象
+        try {
+            FileInputStream fileInputStream = new FileInputStream("box.ser");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            Object one = objectInputStream.readObject();
+
+            Box box2 = (Box) one;
+            System.out.println(box2.height + ", " + box2.width);
+            objectInputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
